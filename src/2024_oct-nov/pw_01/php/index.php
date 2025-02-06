083f74fa9d542f2835b0b7e99f6b90535ce3fe40
viktor <sterilework@gmail.com>
2025-02-06 09:05:15 +0300

    feat: add php file

--- src/2024_oct-nov/pw_01/php/index.php
+++ src/2024_oct-nov/pw_01/php/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practical Work 01 (PHP)</title>
     <link rel="shortcut icon" href="#">
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
     <header></header>
@@ -13,11 +13,17 @@
         <h1>Practical Work 01 Oh</h1>
         <h2 name="task_01">Task 01</h2>
         <ul id="task01">
-
+            <?php
+                $a1 = 5 + 3;
+                $a2 = 5 - 3;
+                echo '<li>a1 = 5 + 3 = <strong>' . $a1 . '</strong></li>';
+                echo "<li>a2 = 5 - 3 = <strong>$a2</strong></li>";
+            ?>
+            <li>a4 = 5 / 3 = <strong><?= $a4 = 5 / 3 ?></strong></li>
         </ul>
     </main>
     <footer>
-        <?= phpversion() ?>
+        <p>Build with PHP <?= phpversion() ?></p>
     </footer>
 </body>
 </html>
