dbe7bc64b1f24b1c58e037b448007795df49741e
viktor <sterilework@gmail.com>
2025-02-12 12:06:05 +0300

    chore: ignore compiled/generated sources

--- src/2024_oct-nov/pw_01/tools/serve-ls.sh
+++ src/2024_oct-nov/pw_01/tools/serve-ls.sh
@@ -4,4 +4,10 @@ DIR_PATH="$(dirname "$0")"
 
 source $DIR_PATH/CONS.sh
 
-printFormat "Launching Live server" $GREEN
+printFormat "Copying resources..." $BLUE
+
+cp -r ./css/ ./front_js/ && cp -r ./js/ ./front_js
+
+printFormat "Launching Live server..." $BLUE
+
+$DIR_PATH/../node_modules/live-server/live-server.js --open=$DIR_PATH/../front_js
